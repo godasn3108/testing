5fb1af958950e5cdecb7584b6580c83af301c665
godasn3108 <112085822+godasn3108@users.noreply.github.com>
2023-10-25 17:19:06 +0530

    Update python.py

--- python.py
+++ python.py
@@ -3,7 +3,7 @@
 import yaml
 
 with open('input.yaml', 'r')as file:
-	data = yaml.safe.load(file, Loader=yaml.FullLoader)
+	data = yaml.safe.load(file)
 
 #Set Output Variable
 print(f"::set-output name=R_VERSION::{data['R_VERSION']}")
