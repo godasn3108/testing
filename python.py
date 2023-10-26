b586e70682244377f7aa3b1e65ae2068f05b8e80
godasn3108 <112085822+godasn3108@users.noreply.github.com>
2023-10-26 17:07:53 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,9 @@
 import yaml
-
+import os
 with open('input.yaml', 'r')as file:
 	data = yaml.safe_load(file)
 
 #Set Output Variable
 output_value = data.get('R_VERSION')
 print(f'::set-output name=output_variable::{output_value}')
+os.environ['RVERSION'] = data['R_VERSION']
