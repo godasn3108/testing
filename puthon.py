625e50754f1215729216222bb7b5aa1a20cc2aa0
godasn3108 <112085822+godasn3108@users.noreply.github.com>
2023-10-26 18:52:57 +0530

    Delete puthon.py

--- puthon.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-with open('input.yaml', 'r')as file:
-	data = yaml.safe.load(file, Loader=yaml.FullLoader)
-
-#Set Output Variable
-print(f"::set-output name=R_VERSION::{data['R_VERSION']}")
