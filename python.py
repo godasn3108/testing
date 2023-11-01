8f2480f76f29e1f96fae0890e3cd837b89b33a22
godasn3108 <112085822+godasn3108@users.noreply.github.com>
2023-11-02 02:50:42 +0530

    Update python.py

--- python.py
+++ python.py
@@ -5,7 +5,7 @@ with open('input.yaml', 'r') as file:
     data = yaml.safe_load(file)
 
 # Convert the YAML content to a string
-yaml_string = yaml.dump(data)
+#yaml_string = yaml.dump(data)
 
 # Set the environment variable
-print(f'::set-output name=setup::{yaml_string}')
+print(f'::set-output name=setup::{data}')
